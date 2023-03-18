f82ccf3d0f427f4ec461dbda4416f3a2127a3554
Deepak <guitarjinny@gmail.com>
2023-03-18 16:54:23 +0530

    BasicAuthNAuth

---
+++ src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [Authorize]
+    public IActionResult Authenticated()
+    {
+        return View();
+    }
+
+    [Authorize]
+    public IActionResult TMS()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(string userId, string password)
+    {
+        if (userId == "deepak" && password == "deepak")
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim("userId", userId));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, "Deepak Bisht"));
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimPrincipal = new ClaimsPrincipal(identity);
+
+
+            await HttpContext.SignInAsync(claimPrincipal);
+
+            return Redirect("/Home/Authenticated");
+        }
+
+        return BadRequest();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return Redirect("/Home/Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
