56e988e03469d8f3db34232ac14ed288bad6447c
Deepak <38551724+Jinnybat@users.noreply.github.com>
2023-04-03 15:38:09 +0530

    enable swagger for all environments
    enable swagger for all environments

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -15,11 +15,11 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
