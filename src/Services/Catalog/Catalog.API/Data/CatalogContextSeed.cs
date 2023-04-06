8f5f3584cbffa09cc2f12909a3264db690bcfc9b
Deepak <38551724+Jinnybat@users.noreply.github.com>
2023-04-06 12:11:25 +0530

    adding new product
    adding new product

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -21,6 +21,16 @@ public class CatalogContextSeed
             {
                 new Product()
                 {
+                    Id = "602d2149e773f2a3990b47f9",
+                    Name = "Oppo Reno 8 Pro",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-1.png",
+                    Price = 800.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
                     Id = "602d2149e773f2a3990b47f5",
                     Name = "IPhone X",
                     Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
